0061218ccd9f0f7c5b02139509238f2577a91536
toma-suzuki <toma.szk1005@gmail.com>
2023-05-15 22:35:57 +0900

    batch setting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 import cron from 'node-cron';
 
 cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
+  const date = new Date();
+  const batchApiKey = process.env.BATCH_API_KEY ?? '';
+
+  console.log(`[${date.toLocaleString()}] launch cron batch...`);
+
+  await fetch('https://localhost:80/remind-batch', {
+    method: 'GET',
+    headers: {
+      key: batchApiKey,
+    },
+  })
+    .then((res) => {
+      if (res.status == 200) {
+        console.log(`[${date.toLocaleString()}] success launch batch!!`);
+      } else {
+        console.log(`[${date.toLocaleString()}] failed launch batch...`);
+      }
+    })
+    .catch((err) => {
+      console.log(
+        `[${date.toLocaleString()}] failed launch batch... error: `,
+        err
+      );
+    });
 });
